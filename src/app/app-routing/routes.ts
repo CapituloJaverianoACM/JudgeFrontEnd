1005fed381199732659733eb9303303aef766784
camiloserr <camiloserrano@javeriana.edu.co>
2018-09-09 05:31:43 -0500

    added problem list module

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { ProfileComponent } from '../profile/profile.component';
 import { SignupComponent } from '../signup/signup.component';
+import { ProblemasComponent } from '../problemas/problemas.component';
 
 export const routes: Routes = [
     { path: 'profile', component: ProfileComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'problems', component: ProblemasComponent},
     { path: '', redirectTo: '/signup', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProblemasComponent } from './problemas/problemas.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ProfileComponent } from './profile/profile.component';
     LoginComponent,
     HeaderComponent,
     SignupComponent,
-    ProfileComponent
+    ProfileComponent,
+    ProblemasComponent
   ],
   imports: [
     BrowserModule,
