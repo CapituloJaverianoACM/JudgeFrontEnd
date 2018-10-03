769b6182f11cd172f876df158bda83268e5c937f
camiloserr <camiloserrano@javeriana.edu.co>
2018-10-03 09:59:26 -0500

    fixed spelling

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/User'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -13,25 +14,26 @@ export class SignupComponent implements OnInit {
   public rePassword: string;
 
 
-  constructor(private userService:UserService) { }
+  constructor(private userService:UserService, 
+    private router: Router) { }
 
   ngOnInit() {
     this.user = new User();
   }
 
-
   onSubmit():void {
     if(this.password === this.rePassword){
       console.log("iguales");
       this.user.password = this.password;
+      this.router.navigate(['problems']); //TODO: check style
     }
     else{
       console.log("paila"); // TODO - Manage error
     }
     let ans = this.userService.registerUser(this.user)
-      .subscribe(response => {
-        console.log(response);
-      });
+    .subscribe(response => {
+      console.log(response);
+    });
     console.log(ans);
     
   }
