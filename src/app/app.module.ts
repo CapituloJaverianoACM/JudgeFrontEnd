01ed3956cb536926696383e052800f4331e84b63
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-01-24 21:24:51 -0500

    Fresh new Proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CustomMaterialModule } from './core/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-
-import 'hammerjs';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProblemasComponent } from './problemas/problemas.component';
-import { ProblemDescriptionComponent } from './problem-description/problem-description.component';
-import { MySubmissionsComponent } from './my-submissions/my-submissions.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HeaderComponent,
-    SignupComponent,
-    ProfileComponent,
-    ProblemasComponent,
-    ProblemDescriptionComponent,
-    MySubmissionsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    CustomMaterialModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
-  entryComponents: [LoginComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/models/Submission.ts
+++
@@ -1,7 +0,0 @@
-export class Submission {
-    public veredict:string;
-    public score: number;
-    public time_stamp:Date;
-    public problem: number;
-    public source_code: File;
-}
