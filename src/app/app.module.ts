1d423564e72839824074b1bb05d111e441d8a99a
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-02-04 21:36:07 -0500

    Finished login front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from "./core/material.module";
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent]
 })
 export class AppModule { }
